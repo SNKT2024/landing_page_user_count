d208111c3e11b0530d35583df99f79da9358264e
sanketkumbharstu@gmail.com <120247634+SNKT2412@users.noreply.github.com>
2024-08-22 17:30:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import "./App.css";
+import { Table } from "react-bootstrap";
+import { db } from "./firebase";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+
+function App() {
+  const [waitlistData, setWaitlistData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userRef = collection(db, "waitlist");
+        const snapshot = await getDocs(userRef);
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setWaitlistData(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h2>Total Count: {waitlistData.length}</h2>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Email</th>
+            <th>TimeStamp</th>
+          </tr>
+        </thead>
+        <tbody>
+          {waitlistData.map((entry, index) => (
+            <tr key={entry.id}>
+              <td>{index + 1}</td>
+              <td>{entry.email}</td>
+              <td>{entry.timestamp?.toDate().toLocaleString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+}
+
+export default App;
